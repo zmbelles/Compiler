5aeb99de62555be572489afbadc781663a4c1d46
zmbelles <91072653+zmbelles@users.noreply.github.com>
2021-09-21 17:25:50 -0500

    Update LexicalAnalyzer.cpp

--- LexicalAnalyzer.cpp
+++ LexicalAnalyzer.cpp
@@ -23,28 +23,30 @@ class LexAnalyzer{
         // other private methods
     public:
 
-    LexAnalyzer(istream& infile){
+    LexAnalyzer(istream& infile){}
         
-        int i=0;
-        while(!infile.eof()){
-            infile >> tokens[i];
-            i++;
-        }
-    }
-        // pre: parameter refers to open data file consisting of token and
-        // lexeme pairs i.e.  s_and and t_begin begin t_int 27.  Each pair    // appears on its own input line.
-        // post: tokenmap has been populated
+    void scanFile(istream& infile, ostream& outfile);
+    // pre: 1st parameter refers to an open text file that contains source
+    // code in the language, 2nd parameter refers to an open empty output
+    // file
+    // post: If no error, the token and lexeme pairs for the given input
+    // file have been written to the output file and the vectors have been // populated.  If there is an error, incomplete token/lexeme pairs are
+    // written to the output file and populated in the vectors.  An error // message is also written to the file. A success or fail message has // printed to the console.
 
-        void scanFile(istream& infile, ostream& outfile);
-        // pre: 1st parameter refers to an open text file that contains source
-        // code in the language, 2nd parameter refers to an open empty output
-        // file
-        // post: If no error, the token and lexeme pairs for the given input
-        // file have been written to the output file and the vectors have been // populated.  If there is an error, incomplete token/lexeme pairs are
-        // written to the output file and populated in the vectors.  An error // message is also written to the file. A success or fail message has // printed to the console.
+};
+
+
+int main () {
+    filebuf fb;
+    cout << "Please enter the name of the file you wish to compile: ";
+    string fileName;
+    cin >> fileName;
+    fb.open(fileName, ios::in);
     
-        };
-
-
-int main() {
+    istream infile(&fb);
+    
+    LexAnalyzer LA(infile);
+    
+    fb.close();
+    return 0;
 }
